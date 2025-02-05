dfcb07b94f2040c025645f53271390ffb3492adc
KDK <one_good07@naver.com>
2025-02-05 10:12:10 +0900

    [iOS] feat: 회원가입 및 로그인 함수 구현 (#27)

---
+++ src/ios/asyncgate_iOS/asyncgate_iOS/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  asyncgate_iOS
+//
+//  Created by kdk on 2/5/25.
+//
+
+// MARK: Model - 회원가입한 User 정보를 담은 모델
+struct User: Codable {
+    let email: String
+    let password: String
+    let name: String
+    let nickname: String
+    let birth: String
+}
