48459bad50cdb9e6105b7a3d957255bcbee52c6a
KDK <one_good07@naver.com>
2025-02-10 21:36:18 +0900

    [iOS] feat: 길드 뷰 기본 틀 잡기 (#27)

--- src/ios/asyncgate_iOS/asyncgate_iOS/View/Guild/GuildMainView.swift
+++ src/ios/asyncgate_iOS/asyncgate_iOS/View/Guild/GuildMainView.swift
@@ -15,19 +15,24 @@ struct GuildMainView: View {
             VStack {
                 HStack {
                     ScrollView {
-                        VStack(alignment: .leading) {
+                        VStack(alignment: .center) {
+                            Button {
+                                //
+                            } label: {
+                                GuildButton(imageName: "message.fill", color: Color.colorGrayImage)
+                            }
+                            
+                            Rectangle()
+                                .frame(width: 40, height: 1)
+                                .foregroundColor(Color(hex: "#282930"))
+                            
+                            // FIXME: 길드 채널 목록 보여주기
+                            
+                            
                             Button {
                                 isShowCreateGuildView.toggle()
                             } label: {
-                                ZStack {
-                                    Circle()
-                                        .frame(width: 46, height: 46)
-                                        .foregroundStyle(Color.colorNewGuildButton)
-                                    
-                                    Image(systemName: "plus")
-                                        .frame(width: 30, height: 30)
-                                        .foregroundStyle(Color.colorGreen)
-                                }
+                                GuildButton(imageName: "plus", color: Color.colorGreen)
                             }
                         }
                     }
@@ -38,20 +43,86 @@ struct GuildMainView: View {
                         RoundedRectangle(cornerRadius: 20)
                             .foregroundStyle(Color.colorBG)
                             .frame(maxHeight: .infinity)
-                    
-                        ScrollView {
-                            VStack {
-                                Text("Hello, World!")
-                            }
-                        }
-                        .frame(maxHeight: .infinity)
+                     
+                                VStack(alignment: .leading) {
+                                    Button {
+                                        
+                                    } label: {
+                                        Text("중꺽마")
+                                            .foregroundStyle(Color.colorWhite)
+                                            .font(Font.pretendardBold(size: 17))
+                                            
+                                        Image(systemName: "chevron.right")
+                                            .resizable()
+                                            .frame(width: 5, height: 6)
+                                            .foregroundColor(Color(hex: "#9597A3"))
+                                    }
+                                    .padding(.top, 20)
+                                    
+                                    HStack {
+                                        Button {
+                                            
+                                        } label: {
+                                            GuildSerachButton
+                                        }
+                                        
+                                        Button {
+                                            
+                                        } label: {
+                                            GuildIconButton(imageName: "person.2.badge.plus.fill", width: 20, height: 14)
+                                        }
+                                        
+                                        Button {
+                                            
+                                        } label: {
+                                            GuildIconButton(imageName: "calendar", width: 16, height: 16)
+                                        }
+                                    }
+                                    
+                                    ScrollView {
+                                        
+                                    Button {
+                                        
+                                    } label: {
+                                        Text("중꺽마")
+                                            .foregroundStyle(Color.colorWhite)
+                                            .font(Font.pretendardBold(size: 17))
+                                            
+                                        Image(systemName: "chevron.right")
+                                            .resizable()
+                                            .frame(width: 5, height: 6)
+                                            .foregroundColor(Color(hex: "#9597A3"))
+                                    }
+                        
+                                }
+                        } // ㄴ
+    
                     }
+                    
                 }
+                .applyGuildBackground()
             }
-            .applyGuildBackground()
+            .fullScreenCover(isPresented: $isShowCreateGuildView) {
+                CreateGuildView()
+            }
         }
-        .fullScreenCover(isPresented: $isShowCreateGuildView) {
-            CreateGuildView()
+    }
+    
+    var GuildSerachButton: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 200, height: 32)
+                .foregroundStyle(Color(hex: "383A43"))
+            HStack {
+                Image(systemName: "magnifyingglass")
+                    .resizable()
+                    .frame(width: 18, height: 18)
+                    .foregroundStyle(Color(hex: "#C7C8CE"))
+             
+                Text("검색하기")
+                    .foregroundStyle(Color(hex: "#C6C7CD"))
+                    .font(Font.pretendardSemiBold(size: 16))
+            }
         }
     }
 }
