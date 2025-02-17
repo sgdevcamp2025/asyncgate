ffcb78a2f22fda12342567502f7cca72694a126f
toychip <dlawnsgud427@naver.com>
2025-02-17 19:09:13 +0900

    [BE] fix: ApiGateway-Server CORS 로깅 테스트1 (#52)

--- src/backend/apigateway-server/src/main/java/com/asyncgate/apigatewayserver/filter/GlobalCorsFilter.java
+++ src/backend/apigateway-server/src/main/java/com/asyncgate/apigatewayserver/filter/GlobalCorsFilter.java
@@ -1,11 +1,11 @@
 package com.asyncgate.apigatewayserver.filter;
 
-import com.asyncgate.apigatewayserver.config.CorsConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -22,17 +22,28 @@ public class GlobalCorsFilter implements GlobalFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
 
-        logger.info(request.getURI().toString());
-        logger.info(request.getMethod().name());
+        logger.info("==== CORS Filter Start ====");
+        logger.info("Request URI: {}", request.getURI());
+        logger.info("Request Method: {}", request.getMethod().name());
+        logger.info("Request Headers: {}", request.getHeaders());
 
-        if (request.getMethod().matches(OPTIONS_HEADERS)) {
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod().name())) {
             exchange.getResponse().getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, ALLOWED_ORIGIN);
             exchange.getResponse().getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, String.join(", ", ALLOWED_METHODS));
             exchange.getResponse().getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, ALLOWED_HEADERS);
             exchange.getResponse().getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, ALLOW_CREDENTIALS);
+
+            logger.info("Pre-flight OPTIONS request detected.");
+            logger.info("Response Headers: {}", exchange.getResponse().getHeaders());
+
+            exchange.getResponse().setStatusCode(HttpStatus.NO_CONTENT);
             return exchange.getResponse().setComplete();
         }
 
-        return chain.filter(exchange);
+        return chain.filter(exchange).doOnSuccess(aVoid -> {
+            logger.info("==== CORS Filter End ====");
+            logger.info("Response Headers: {}", exchange.getResponse().getHeaders());
+        });
     }
 }
+
