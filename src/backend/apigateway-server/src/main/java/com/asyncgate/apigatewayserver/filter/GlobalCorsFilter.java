d9ad6d3ba8dbdd1da9937e126950fc1d007aaebc
toychip <dlawnsgud427@naver.com>
2025-02-17 17:29:48 +0900

    [BE] fix: Api-Gateway Server CORS open (#48)

---
+++ src/backend/apigateway-server/src/main/java/com/asyncgate/apigatewayserver/filter/GlobalCorsFilter.java
@@ -0,0 +1,31 @@
+package com.asyncgate.apigatewayserver.filter;
+
+import com.asyncgate.apigatewayserver.config.CorsConfig;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import static com.asyncgate.apigatewayserver.config.CorsConfig.*;
+
+@Component
+public class GlobalCorsFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+
+        if (request.getMethod().matches(OPTIONS_HEADERS)) {
+            exchange.getResponse().getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, ALLOWED_ORIGIN);
+            exchange.getResponse().getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, String.join(", ", ALLOWED_METHODS));
+            exchange.getResponse().getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, ALLOWED_HEADERS);
+            exchange.getResponse().getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, ALLOW_CREDENTIALS);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+}
