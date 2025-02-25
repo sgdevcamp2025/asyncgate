84cefb6a5c306cca7522ae337473297b8f7f0ca4
toychip <dlawnsgud427@naver.com>
2025-02-25 21:48:16 +0900

    [BE] test: Chat-server 오류 테스트17 (#77)

--- src/backend/chat-server/src/main/kotlin/com/asyncgate/chat_server/support/utility/CustomSecurityContext.kt
+++ src/backend/chat-server/src/main/kotlin/com/asyncgate/chat_server/support/utility/CustomSecurityContext.kt
@@ -7,10 +7,7 @@ import org.springframework.messaging.Message
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor
 
 object CustomSecurityContext {
-    private const val JWT_HEADER = "jwt-token"
-
-    private const val AUTH_HEADER = "Authorization"
-    private const val BEARER_PREFIX = "Bearer "
+    private const val JWT_HEADER = "Sec-WebSocket-Protocol"
 
     fun extractJwtTokenForStomp(message: Message<*>): String {
         val accessor = StompHeaderAccessor.wrap(message)
@@ -20,13 +17,9 @@ object CustomSecurityContext {
     }
 
     fun extractJwtTokenForHttp(request: HttpServletRequest): String {
-        val authHeader = request.getHeader(AUTH_HEADER)
+        val jwtToken = request.getHeader(JWT_HEADER)
             ?: throw ChatServerException(FailType.JWT_INVALID_TOKEN)
 
-        if (!authHeader.startsWith(BEARER_PREFIX)) {
-            throw ChatServerException(FailType.JWT_INVALID_TOKEN)
-        }
-
-        return authHeader.removePrefix(BEARER_PREFIX).trim()
+        return jwtToken.trim()
     }
 }
