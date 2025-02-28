4cf2273ac6728efbcfb25bfc87a0df7492093e2e
zelkovaria <hyein396@gmail.com>
2025-02-28 23:20:01 +0900

    feat: 빈 채널 레이아웃에 참여중인 채널명 출력 (#74)

--- src/frontend/src/pages/VideoPage/index.tsx
+++ src/frontend/src/pages/VideoPage/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import { useChannelInfoStore } from '@/stores/channelInfo';
 import { BodyRegularText, TitleText1 } from '@/styles/Typography';
 
 import * as S from './styles';
@@ -7,13 +8,15 @@ import * as S from './styles';
 const VideoPage = () => {
   const [isAttend, setIsAttend] = useState(false);
 
+  const { selectedChannel } = useChannelInfoStore();
+
   return (
     <S.VideoPage>
       {isAttend ? (
         <>참여시 비디오들</>
       ) : (
         <S.EmptyParticipant>
-          <TitleText1>채널 이름</TitleText1>
+          <TitleText1>{selectedChannel?.name}</TitleText1>
           <BodyRegularText>현재 음성 채널에 아무도 없어요</BodyRegularText>
         </S.EmptyParticipant>
       )}
