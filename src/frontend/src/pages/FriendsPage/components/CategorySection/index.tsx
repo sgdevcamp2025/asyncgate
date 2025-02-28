17ad368e61aa40d8f37e3f20e019159f512604d1
zelkovaria <hyein396@gmail.com>
2025-03-01 03:27:46 +0900

    [FE] feat: 음성 채널 참여시에만 VoiceChannelController 컴포넌트 노출 (#74)

--- src/frontend/src/pages/FriendsPage/components/CategorySection/index.tsx
+++ src/frontend/src/pages/FriendsPage/components/CategorySection/index.tsx
@@ -2,17 +2,19 @@ import DirectMessageCategory from '@/components/friend/DirectMessageCategory';
 import GuildCategory from '@/components/guild/GuildCategory';
 import VoiceChannelController from '@/components/guild/VoiceChannelController';
 import UserProfile from '@/pages/FriendsPage/components/UserProfile';
+import { useChannelActionStore } from '@/stores/channelAction';
 import { useGuildInfoStore } from '@/stores/guildInfo';
 
 import * as S from './styles';
 
 const CategorySection = () => {
   const { guildId } = useGuildInfoStore();
+  const { isInVoiceChannel } = useChannelActionStore();
 
   return (
     <S.CategorySectionContainer>
       <S.CategoryItemWrapper>{guildId ? <GuildCategory /> : <DirectMessageCategory />}</S.CategoryItemWrapper>
-      <VoiceChannelController />
+      {isInVoiceChannel && <VoiceChannelController />}
       <UserProfile
         userName="Fe"
         userImageUrl=""
